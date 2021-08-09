093b2e912b292abc195fa416398d318c55eb347f
Fus Przemyslaw <cubical97@gmail.com>
2021-08-09 18:47:04 +0200

    Read inputs in frontend - create places

--- meetings-and-events/ClientApp/src/app/place/create/create-meeting/create-place-meeting.component.ts
+++ meetings-and-events/ClientApp/src/app/place/create/create-meeting/create-place-meeting.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {ValueConverter} from "@angular/compiler/src/render3/view/template";
 
 @Component({
   selector: 'app-create-place-meeting',
@@ -6,17 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-place-meeting.component.css']
 })
 export class CreatePlaceMeetingComponent {
-  model;
-
-  inputCountry: string;
-  inputCity: string;
-  inputStreet: string;
-  inputNumber: string;
-
-  inputDescription: string;
-
   errorMessage: string;
-  
-  
-  
+
+  create(form: NgForm) {
+    const credentials = {
+      'country': form.value.country,
+      'city': form.value.city,
+      'street': form.value.street,
+      'number': form.value.number,
+      'description': form.value.description,
+      'datepicker': form.value.dp,
+      'timeOC': [form.value.timeOpen, form.value.timeClose]
+    }
+    
+    // TODO validation
+    
+    // TODO upload
+    console.error(credentials);
+  }
 }
--- meetings-and-events/ClientApp/src/app/place/create/create-place/create-place-place.component.ts
+++ meetings-and-events/ClientApp/src/app/place/create/create-place/create-place-place.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-create-place-place',
@@ -6,15 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-place-place.component.css']
 })
 export class CreatePlacePlaceComponent {
-  inputCountry: string;
-  inputCity: string;
-  inputStreet: string;
-  inputNumber: string;
-
-  inputDescription: string;
-
   errorMessage: string;
-  
-  
-  
+
+  create(form: NgForm) {
+    const credentials = {
+      'country': form.value.country,
+      'city': form.value.city,
+      'street': form.value.street,
+      'number': form.value.number,
+      'description': form.value.description,
+      'timeOC1': [form.value.time1check, form.value.time1Open, form.value.time1Close],
+      'timeOC2': [form.value.time2check, form.value.time2Open, form.value.time2Close],
+      'timeOC3': [form.value.time3check, form.value.time3Open, form.value.time3Close],
+      'timeOC4': [form.value.time4check, form.value.time4Open, form.value.time4Close],
+      'timeOC5': [form.value.time5check, form.value.time5Open, form.value.time5Close],
+      'timeOC6': [form.value.time6check, form.value.time6Open, form.value.time6Close],
+      'timeOC7': [form.value.time7check, form.value.time7Open, form.value.time7Close]
+    }
+
+    // TODO validation
+
+    // TODO upload
+    console.error(credentials);
+  }
 }
